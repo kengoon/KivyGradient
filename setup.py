44d953b822c760f0b47bf3c127a13bfe9af50b1c
kengoon <kengoon19@gmail.com>
2021-09-06 16:39:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='KivyGradient',
+    version='0.0.1',
+    packages=['kivy_gradient'],
+    url='https://github.com/kengoon/KivyGradient',
+    license='MIT',
+    author='kengo',
+    author_email='kengoon19@gmail.com',
+    description='KivyGradient allows you to add a gradient color to your Kivy Widget'
+)

87140c7d8acdddfcabaf83d75731502e739d2726
Lucas Figueira <lucasbf@gmail.com>
2025-07-28 22:51:18 -0300

    Add gradient texture creation functionality
    - Implemented methods for creating textures with linear, horizontal, vertical, and angled gradients.
    - Added parameters for color, size, and angle to customize gradient textures.
    - Enhanced documentation for gradient texture methods.

--- README.md
+++ README.md
@@ -79,4 +79,35 @@ Test().run()
 
 ```
 
+```python
+from kivy.app import App
+from kivy.lang import Builder
+
+
+kv = """
+#:import get_color_from_hex kivy.utils.get_color_from_hex
+#:import Gradient kivy_gradient.Gradient
+RelativeLayout:
+    BoxLayout
+        id: box
+        on_kv_post: print(get_color_from_hex("E91E63"))
+        canvas:
+            Rectangle:
+                size: self.size
+                pos: self.pos
+                texture: Gradient.angled([get_color_from_hex("ec008c"), get_color_from_hex("00aeef")], size=self.size, angle=135)
+"""
+
+
+class Test(App):
+    def build(self):
+        return Builder.load_string(kv)
+
+    def on_stop(self):
+        self.root.ids.box.export_to_png("gradient.png")
+        
+
+Test().run()
+```
+
 ![gradient](https://user-images.githubusercontent.com/42192162/132247485-bb48a2ed-ff48-4388-8fff-68a7eb11f69e.png)
--- kivy_gradient/__init__.py
+++ kivy_gradient/__init__.py
@@ -1,7 +1,7 @@
 from itertools import chain
+from math import cos, sin, radians
 
 from kivy.graphics.texture import Texture
-
 
 class Gradient(object):
 
@@ -18,4 +18,48 @@ class Gradient(object):
         texture = Texture.create(size=(1, len(args)), colorfmt='rgba')
         buf = bytes([int(v * 255) for v in chain(*args)])  # flattens
         texture.blit_buffer(buf, colorfmt='rgba', bufferfmt='ubyte')
+        return texture
+        
+    @staticmethod
+    def angled(colors, size=(256, 256), angle=0):
+        """
+        Creates a texture with a gradient at an arbitrary angle.
+        :param colors: List of colors (r, g, b, a) between 0.0 and 1.0.
+        :param size: (width, height) of the texture.
+        :param angle: Angle of the gradient in degrees (0 = left to right).
+        :return: Texture
+        """
+        w, h = size
+        texture = Texture.create(size=size, colorfmt='rgba')
+        texture.mag_filter = 'linear'
+
+        # Direção do gradiente
+        angle_rad = radians(angle)
+        dx = cos(angle_rad)
+        dy = sin(angle_rad)
+
+        buf = bytearray(w * h * 4)
+        num_colors = len(colors) - 1
+
+        for y in range(h):
+            for x in range(w):
+                # Normaliza posição (0 a 1) na direção do vetor (dx, dy)
+                u = (x / w - 0.5) * dx + (y / h - 0.5) * dy + 0.5
+                u = min(max(u, 0), 1)
+
+                # Encontra as duas cores entre as quais interpolar
+                pos = u * num_colors
+                i = int(pos)
+                frac = pos - i
+                c1 = colors[i]
+                c2 = colors[min(i + 1, num_colors)]
+
+                rgba = [
+                    int(255 * (c1[j] + frac * (c2[j] - c1[j])))
+                    for j in range(4)
+                ]
+                index = 4 * (y * w + x)
+                buf[index:index + 4] = bytes(rgba)
+
+        texture.blit_buffer(bytes(buf), colorfmt='rgba', bufferfmt='ubyte')
         return texture

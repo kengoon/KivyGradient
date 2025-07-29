42896db8f8e167abcf6637a8d9c36516ba62bce9
Lucas Baggio Figueira <lucasbf@gmail.com>
2025-07-28 22:55:09 -0300

    Update kivy_gradient/__init__.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- kivy_gradient/__init__.py
+++ kivy_gradient/__init__.py
@@ -55,7 +55,7 @@ class Gradient(object):
                 c2 = colors[min(i + 1, num_colors)]
 
                 rgba = [
-                    int(255 * (c1[j] + frac * (c2[j] - c1[j])))
+                    int(255 * min(max(c1[j] + frac * (c2[j] - c1[j]), 0.0), 1.0))
                     for j in range(4)
                 ]
                 index = 4 * (y * w + x)
